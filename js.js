ebc990ede44dc3d450ea3dc5145af92937abbd5d
shrouk elian <shroukelian552@gmail.com>
2025-09-15 01:44:13 +0300

    Updated

--- js.js
+++ js.js
@@ -3,12 +3,10 @@ const navLinks = document.querySelector('.nav-links');
 const allLinks = document.querySelectorAll('.nav-links a');
 
 if (menuIcon && navLinks) {
-    // لفتح وإغلاق القائمة عند الضغط على الأيقونة
     menuIcon.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
 
-    // لإغلاق القائمة عند الضغط على أي رابط بداخلها
     allLinks.forEach(link => {
         link.addEventListener('click', () => {
             navLinks.classList.remove('active');
