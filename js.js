6e2cb70b1aebb45c8146cfe60736cbcbc19c63bc
shrouk elian <shroukelian552@gmail.com>
2025-09-14 22:49:12 +0300

    first commit

--- js.js
+++ js.js
@@ -1,18 +1,17 @@
+const menuIcon = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.nav-links');
+const allLinks = document.querySelectorAll('.nav-links a');
 
-    const menuIcon = document.querySelector('.menu-icon');
-    const navLinks = document.querySelector('.nav-links');
-    const allLinks = document.querySelectorAll('.nav-links a'); // <<-- أضفنا هذا
+if (menuIcon && navLinks) {
+    // لفتح وإغلاق القائمة عند الضغط على الأيقونة
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
 
-    if (menuIcon && navLinks) {
-        // لفتح وإغلاق القائمة عند الضغط على الأيقونة
-        menuIcon.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
+    // لإغلاق القائمة عند الضغط على أي رابط بداخلها
+    allLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
         });
-
-        // لإغلاق القائمة عند الضغط على أي رابط بداخلها
-        allLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                navLinks.classList.remove('active');
-            });
-        });
-    }
+    });
+}
